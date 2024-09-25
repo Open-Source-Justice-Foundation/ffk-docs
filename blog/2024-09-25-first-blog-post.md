b9c32f94773e43b28d6ef97053f2e3f734d48271
Tommy Volk <tvolk131@gmail.com>
2024-09-25 11:59:35 -0500

    feat: initial docusaurus website

---
+++ blog/2024-09-25-first-blog-post.md
@@ -0,0 +1,12 @@
+---
+slug: first-blog-post
+title: First Blog Post
+authors: [aarondaniel]
+tags: [hello, welcome]
+---
+
+Hello there!
+
+<!-- truncate -->
+
+This is an example blog post.
---
+++ docusaurus.config.ts
@@ -0,0 +1,117 @@
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
+const config: Config = {
+  title: "Foundation Formation Kit",
+  tagline: "By the Open Source Justice Foundation",
+  favicon: "img/favicon.ico",
+
+  url: "https://ffk.opensourcejustice.org",
+  baseUrl: "/",
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          sidebarPath: "./sidebars.ts",
+          editUrl:
+            "https://github.com/Open-Source-Justice-Foundation/ffk-website",
+        },
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ["rss", "atom"],
+            xslt: true,
+          },
+          editUrl:
+            "https://github.com/Open-Source-Justice-Foundation/ffk-website",
+          onInlineTags: "warn",
+          onInlineAuthors: "warn",
+          onUntruncatedBlogPosts: "warn",
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // TODO: Replace with our logo.
+    image: "img/docusaurus-social-card.jpg",
+    navbar: {
+      title: "Foundation Formation Kit",
+      logo: {
+        alt: "Foundation Formation Kit Logo",
+        src: "img/logo.svg",
+      },
+      items: [
+        {
+          type: "docSidebar",
+          sidebarId: "tutorialSidebar",
+          position: "left",
+          label: "Tutorial",
+        },
+        { to: "/blog", label: "Blog", position: "left" },
+        {
+          href: "https://github.com/Open-Source-Justice-Foundation/ffk-website",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "Tutorial",
+              to: "/docs/intro",
+            },
+          ],
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Discord",
+              href: "https://discord.gg/FYkpPB8meW",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "Blog",
+              to: "/blog",
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/Open-Source-Justice-Foundation/ffk-website",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Open Source Justice Foundation.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
