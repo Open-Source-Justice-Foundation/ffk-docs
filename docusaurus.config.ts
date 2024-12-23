c7f5a4739ca9aa2b87836966cba838c0152c9335
J the Code Monkey <jaythecodemonkey.7vk7i@slmail.me>
2024-12-23 18:51:23 -0500

    fix: update tutorial to docs

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -59,9 +59,9 @@ const config: Config = {
       items: [
         {
           type: "docSidebar",
-          sidebarId: "tutorialSidebar",
+          sidebarId: "docsSidebar",
           position: "left",
-          label: "Tutorial",
+          label: "Docs",
         },
         { to: "/blog", label: "Blog", position: "left" },
         {
@@ -78,7 +78,7 @@ const config: Config = {
           title: "Docs",
           items: [
             {
-              label: "Tutorial",
+              label: "Docs",
               to: "/docs/intro",
             },
           ],
